5959654ddf056037571138ae6cacda5883c51bbc
Marco Ng <marcong95@163.com>
2017-11-16 00:31:08 +0800

    add CommitTimer to App.vue install axios, bulma, moment

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ import 'normalize.css'
 import './styles/base.styl'
 
 import Vue from 'vue'
+import axios from 'axios'
+
 import App from './App.vue'
+// import store from './store'
+
+Vue.http = Vue.prototype.$http = axios
 
 var vm = new Vue({
   el: '#container',
-  render: h => h(App)
+  render: h => h(App),
+  // store
 })
